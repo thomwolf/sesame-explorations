8942e32f0fcb550c270b9ec94027ee3c153f6652
Thomas Wolf <thomas@huggingface.co>
2025-04-28 12:27:22 +0200

    update

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# sesame-explorations
+# Sesame-explorations
+
+This is the companion codebase to my short explorations around the Sesame model.
+
+The introduction blog post is here: https://thomwolf.io/blog/speech-ai.html
+
+## Current status
+
+Currently preparing the training dataset from a (4 hours) NotebookLM dataset: https://huggingface.co/datasets/thomwolf/notebooklm-sample
+
+Current processing scripts are in this folder: [./scripts](./scripts)
+
+    [✅] Extract and convert audio
+    [✅] Diarize dataset
+    [ ] Audio and text tokenization
+    [ ] Finetuning CSM
--- scripts/split-and-tokenize-data.py
+++ scripts/split-and-tokenize-data.py
@@ -4,20 +4,25 @@ import pandas as pd
 import numpy as np
 import sphn
 
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor
+from datasets import load_dataset
+
 from huggingface_hub import hf_hub_download
 import torch
-from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor
+import torch.functional as F
 
 from moshi.models import loaders, LMGen
 
-mimi_weight = hf_hub_download(loaders.DEFAULT_REPO, loaders.MIMI_NAME)
-mimi = loaders.get_mimi(mimi_weight, device='cuda' if torch.cuda.is_available() else 'cpu')
-mimi.set_num_codebooks(32)  # up to 32 for mimi
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+# Setup Mimi
+mimi_weight = hf_hub_download(loaders.DEFAULT_REPO, loaders.MIMI_NAME, token=True)
+mimi = loaders.get_mimi(mimi_weight, device=device)
+mimi.set_num_codebooks(32)  # up to 32 for mimi
 pcm_chunk_size = int(mimi.sample_rate / mimi.frame_rate)
 mimi_sample_rate = mimi.sample_rate
 
-device = "cuda:0" if torch.cuda.is_available() else "cpu"
+# Setup Whisper
 torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 model_id = "openai/whisper-large-v3-turbo"
 model = AutoModelForSpeechSeq2Seq.from_pretrained(
@@ -25,7 +30,15 @@ model = AutoModelForSpeechSeq2Seq.from_pretrained(
 )
 model.to(device)
 processor = AutoProcessor.from_pretrained(model_id)
-whisper_sample_rate = 16000
+
+dataset = load_dataset("distil-whisper/librispeech_long", "clean", split="validation")
+sample = dataset[0]["audio"]
+
+input_features = processor(
+    sample["array"], return_tensors="pt", truncation=False, sampling_rate=sample["sampling_rate"]
+).input_features
+input_features = input_features.to(device, torch_dtype)
+
 generate_kwargs = {
     "language": "en",
     "max_new_tokens": 445,
@@ -39,8 +52,17 @@ generate_kwargs = {
     "return_segments": True,
 }
 
+generated_ids = model.generate(input_features, **generate_kwargs)
+segments = generated_ids["segments"][0]
 
-def tokenize_audio(input_folder, output_file):
+for segment in segments:
+    text = processor.decode(segment["tokens"], skip_special_tokens=True)
+    print(f"start: {segment['start']}, end: {segment['end']}, text: {text}")
+
+
+
+
+def load_and_split_audio(input_folder, output_file=None):
     """
     Load all .rttm files in the input folder, split the audio files into chunks, and tokenize the chunks.
     
@@ -48,17 +70,18 @@ def tokenize_audio(input_folder, output_file):
         input_folder (str): Path to the folder containing .rttm files
     """
     # Get all .rttm files in the folder
-    rttm_files = glob.glob(os.path.join(input_folder, "*.rttm"))
+    rttm_files = ["small.rttm"]  # glob.glob(os.path.join(input_folder, "*.rttm"))
 
     rttm_files = ["/home/thomwolf/Documents/Github/notebooklm-sample/small.rttm"]
     output_file = "/home/thomwolf/Documents/Github/notebooklm-sample/small.rttm"
 
     for rttm_file in rttm_files:
+        full_rttm_file = os.path.join(input_folder, rttm_file)
         # Load the rttm file in a pandas dataframe (separator is a space).
         # Load the rttm file in a pandas dataframe
         # The first column is the type, the second is the file id, the third is the channel id, the fourth is the turn onset, the fifth is the turn duration, the sixth is the orthography field, the seventh is the speaker type, the eighth is the speaker name, the ninth is the confidence score, and the tenth is the signal lookahead time.
         # We have no header, so we need to specify the header names.
-        df = pd.read_csv(rttm_file, sep="\t", header=None, names=["type", "file_id", "channel_id", "turn_onset", "turn_duration", "orthography_field", "speaker_type", "speaker_name", "confidence_score", "signal_lookahead_time"])
+        df = pd.read_csv(full_rttm_file, sep=" ", header=None, names=["type", "file_id", "channel_id", "turn_onset", "turn_duration", "orthography_field", "speaker_type", "speaker_name", "confidence_score", "signal_lookahead_time"])
 
         # Load the wav files and split it based on the turn onsets and turn durations.
         # Turn Onset -- onset of turn in seconds from beginning of recording
@@ -68,70 +91,47 @@ def tokenize_audio(input_folder, output_file):
         # we change the extension of the file to .wav:
         wav_file = rttm_file.replace(".rttm", ".wav")
         sample_pcm, sample_pcm_source_rate = sphn.read(wav_file)
-        print("loaded pcm", sample_pcm.shape, sample_pcm_source_rate)
-        mimi_sample_pcm = sphn.resample(
-            sample_pcm, src_sample_rate=sample_pcm_source_rate, dst_sample_rate=mimi_sample_rate
-        )
-        whisper_sample_pcm = sphn.resample(
-            sample_pcm, src_sample_rate=sample_pcm_source_rate, dst_sample_rate=whisper_sample_rate
-        )
-        mimi_sample_pcm = torch.tensor(mimi_sample_pcm, device=device)
-        whisper_sample_pcm = torch.tensor(whisper_sample_pcm, device=device)
-        # add batch dimension and change from 2 channels to 1 channel by averaging the channels
-        mimi_sample_pcm = mimi_sample_pcm[None].to(device=device)
-        mimi_sample_pcm = mimi_sample_pcm.mean(dim=1, keepdim=True)
+        print("loaded pcm", sample_pcm.shape[-1] / sample_pcm_source_rate)
 
-        # max_duration_len = int(mimi_sample_rate * max_duration_sec)
-        # if sample_pcm.shape[-1] > max_duration_len:
-        #     sample_pcm = sample_pcm[..., :max_duration_len]
-        print("resampled pcm", mimi_sample_pcm.shape, mimi_sample_rate)
-        # sample_pcm = sample_pcm[None].to(device=device)
-
-        with torch.no_grad():
-            codes = mimi.encode(mimi_sample_pcm)  # [B, K = 32, T]
-        
-        print("codes", codes.shape)
-        # splits the code in chunks for each speaker based on the turn onsets and turn durations as well as the pcm_chunk_size
+        # Extract speaker turns from wav
+        dataset = []
         for i in range(len(df)):
+            speaker_id = df['speaker_name'][i]
             turn_onset = df['turn_onset'][i]
             turn_duration = df['turn_duration'][i]
-            chunk_start = int(turn_onset * mimi_sample_rate)
-            chunk_end = int((turn_onset + turn_duration) * mimi_sample_rate)
-            chunk = codes[:, :, chunk_start:chunk_end]
-            print("chunk", chunk.shape)
+            chunk_start = int(turn_onset * sample_pcm_source_rate)
+            chunk_end = int((turn_onset + turn_duration) * sample_pcm_source_rate)
 
-            # split the whisper chunks
-            chunk_start = int(turn_onset * whisper_sample_rate)
-            chunk_end = int((turn_onset + turn_duration) * whisper_sample_rate)
-            chunk_whisper = whisper_sample_pcm[:, chunk_start:chunk_end]
-            print("chunk whisper", chunk_whisper.shape)
-            # encode the chunk with the whisper model
-            input_features = processor(
-                chunk_whisper.cpu().numpy(), return_tensors="pt", truncation=False, sampling_rate=whisper_sample_rate
-            ).input_features
-            input_features = input_features.to(device=device, dtype=torch_dtype)
-            # generate the tokens
-            generated_ids = model.generate(input_features, **generate_kwargs)
-            # decode the tokens
-            transcription = processor.batch_decode(generated_ids, skip_special_tokens=True)
-            # Gather text from the chunks
-            text = []
-            for segment in generated_ids["segments"][0]:
-                text.append(processor.decode(segment["tokens"], skip_special_tokens=True))
-            # print the text
-            print(f"speaker {df['speaker_name'][i]}: {text}")
-            # save the text to a file
-            with open(os.path.join(output_file, f"{df['file_id'][0]}_{i}.txt"), "w") as f:
-                for t in text:
-                    f.write(t + "\n")
-            # save the codes to a file
-            np.save(os.path.join(output_file, f"{df['file_id'][0]}_{i}.npy"), chunk.cpu().numpy())
+            # if (chunk_end - chunk_start}/sample_pcm_source_rate * mimi.frame_rate % 0 != 0:
+            #  Make it a multiple of mimi.frame_rate
 
+            turn_pcm = sample_pcm[:, chunk_start:chunk_end]
 
-            # test decoding the chunk and write it to a wav file
-            decoded_pcm = mimi.decode(chunk)
-            sphn.write_wav(os.path.join(output_file, f"{df['file_id'][0]}_{i}.wav"), decoded_pcm.cpu().numpy(), mimi_sample_rate)
-            print(f"wrote {df['file_id'][0]}_{i}.wav with shape {decoded_pcm.shape} and length {decoded_pcm.shape[-1] / mimi_sample_rate} seconds for speaker {df['speaker_name'][i]}")
+            # Resample and make mono
+            turn_pcm = sphn.resample(
+                turn_pcm, src_sample_rate=sample_pcm_source_rate, dst_sample_rate=mimi_sample_rate
+            )
+            # Make it mono by averaging the channels.
+            turn_pcm = np.mean(turn_pcm, axis=0)
+            turn_pcm = torch.tensor(turn_pcm, device=device)
+
+            with torch.no_grad():
+                codes = mimi.encode(turn_pcm)  # [B, K = 32, T]
+                text = whisper.encode(turn_pcm)
+            
+            if speaker_id == "SPEAKER_00":
+                speaker_int = 0
+            elif speaker_id == "SPEAKER_01":
+                speaker_int = 1
+            else:
+                raise ValueError() 
+            dataset.append((speaker_int, text.squeeze(dim=0), codes.squeeze(dim=0)))
+
+        # max_length = max(turn.shape[-1] for turn in speaker_turns)
+        # speaker_turns = [F.pad(turn, (0, max_length - turn.shape[-1])) for turn in speaker_turns]
+        # batch_turns = torch.stack(speaker_turns, dim=0).to(device=device)
+        # with torch.no_grad():
+        #     batch_codes = mimi.encode(batch_turns)  # [B, K = 32, T]
 
 
         ## WARNING: When streaming, make sure to always feed a total amount of audio that is a multiple
@@ -150,7 +150,12 @@ if __name__ == "__main__":
     
     # args = parser.parse_args()
     
-    tokenize_audio("", "")
+    # load_and_split_audio(args.input_folder, args.output_file)
+
+    load_and_split_audio(input_folder="../notebooklm-sample", output_file="../notebooklm-sample/output")
+
+
+
 
 # Example usage
 # python split-an.py /path/to/your/folder output.rttm
